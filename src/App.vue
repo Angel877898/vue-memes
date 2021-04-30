980e509736b553f049cd539cc51a0dceaa3cd0d9
Angel877898 <angurie_2008@hotmail.com>
2021-04-30 15:18:20 -0500

    added* ListMemes and Meme

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <button type="button" class="btn btn-primary">Primary</button>
+  <div class="container">
+    <ListMemes />
+  </div>
 </template>
 
 <script>
+import ListMemes from "./components/ListMemes";
 import { useStore } from "vuex";
 
 export default {
   name: "App",
+  components: {
+    ListMemes,
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,23 @@ export default createStore({
     titleApp: "Memes",
     memes: [],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setMemes(state, payload) {
+      state.memes = payload;
+    },
+  },
+  actions: {
+    async getMemes({ commit }) {
+      try {
+        const response = await fetch("https://api.imgflip.com/get_memes");
+        const result = await response.json();
+
+        commit("setMemes", result.data.memes);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   modules: {},
   getters: {},
 });
